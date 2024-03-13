37c25be7b4da1fb6a57f5fc5dc907207c40a18b0
Connor Burbridge <connorburbridge@pp787b8ad19254.usask.ca>
2024-03-13 10:31:53 -0600

    adding initial figures for gene length histograms. still need titles and maybe set a max ylim for all plots

--- results/gene-lengths/length-analysis.py
+++ results/gene-lengths/length-analysis.py
@@ -8,6 +8,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy import stats
 import random
+import seaborn as sns
+
+# Nice color palette for colorblind individuals
+#CB_color_cycle = ['#377eb8', '#ff7f00', '#4daf4a',
+#                  '#f781bf', '#a65628', '#984ea3',
+#                  '#999999', '#e41a1c', '#dede00']
 
 def main(args):
     print(args)
@@ -28,27 +34,32 @@ def main(args):
             print(tools[i] + ' ' + tools[j])
             pvalues.append(stats.ks_2samp(cds[i], cds[j]).pvalue)
 
-    fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
-    axes = [ax1, ax2, ax3]
-
-    for i in range(0, len(axes)):
-        axes[i].hist(cds[i], bins=1000)
-        axes[i].set_xlim(0,5000)
-        axes[i].set_ylim(0, 500)
-        axes[i].set_xlabel(tools[i])
-
-    ax2.set_yticks([])
-    plt.suptitle(args.strain)
-    plt.ylim(0, 500)
-    plt.tight_layout()
-    plt.show()
     print(pvalues)
+    #fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
+    #axes = [ax1, ax2, ax3]
 
+    #for i in range(0, len(cds)):
+    #    axes[i].hist(cds[i], bins=1000)
+    #    axes[i].set_xlim(0,5000)
+    #    axes[i].set_ylim(0, 500)
+    #    axes[i].set_xlabel(tools[i])
+
+    #ax2.set_yticks([])
+    size = 75
+    sns.histplot(cds[0], bins=range(0, max(cds[0]) + size, size), kde=True, color='#e41a1c', label=tools[0])
+    sns.histplot(cds[1], bins=range(0, max(cds[0]) + size, size), kde=True, color='#377eb8', label=tools[1])
+    sns.histplot(cds[2], bins=range(0, max(cds[0]) + size, size), kde=True, color='#dede00', label=tools[2])
+    #plt.ylim(0, 600)
+    plt.xlim(0, 5500)
+    plt.xlabel('Gene Length (bp)')
+    plt.tight_layout()
+    plt.legend()
+    plt.savefig(args.output)
+    plt.show()
     
 def parseData(cdsPath):
     cdsArray = []
     for cds in SeqIO.parse(cdsPath, "fasta"):
-        #print(cds.id + '\t' + str(len(cds.seq)))
         cdsArray.append(len(cds.seq))
 
     return(cdsArray)
@@ -59,7 +70,7 @@ if __name__ == "__main__":
     parser.add_argument('-b', type=str, dest='braker', help='Path to CDS from Braker2')
     parser.add_argument('-r', type=str, dest='refseq', help='Path to CDS from RefSeq annotation')
     parser.add_argument('-g', type=str, dest='genemark', help='Path to CDS from GeneMark')
-    parser.add_argument('-s', type=str, dest='strain', help='Nmae of Trichoderma strain')
+    parser.add_argument('-o', type=str, dest='output', help='Output file name')
     args = parser.parse_args()
     
     
